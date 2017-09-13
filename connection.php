0b6e872fce376e889458d2bee25d4c66b21644f9
Samakunchan <samakunchan@gmail.com>
2017-09-13 18:14:35 +0200

    Création de la page de connection en Html

--- connection.php
+++ connection.php
@@ -1,7 +1,38 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Samakunchan
- * Date: 13/09/2017
- * Time: 17:52
- */
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Site de Samkunchan</title>
+</head>
+<body>
+<header>
+    <h1>Site de Samakunchan</h1>
+    <h3>Page principale</h3>
+</header>
+<section>
+    <article>
+        <form action="traitement.php" method="post">
+            <label>Veuillez inscrire votre pseudo</label>
+            <p>
+                <input type="text" name="pseudo" placeholder="ex : Samakunchan">
+            </p>
+            <label>Mot de passe</label>
+            <p>
+                <input type="password" name="password">
+            </p>
+            <p>
+                <input type="submit" value="Connection">
+            </p>
+        </form>
+    </article>
+</section>
+<section id="contenu">
+    <p>Formulaire de connection</p>
+</section>
+
+</body>
+</html>
